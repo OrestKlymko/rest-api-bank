1b6c034e6debac3f5c54f7f29bfdc4778c3dd9c1
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-08-06 09:26:48 +0200

    upd test

--- src/test/java/com/bank/api/customer/service/CustomerServiceTest.java
+++ src/test/java/com/bank/api/customer/service/CustomerServiceTest.java
@@ -35,33 +35,26 @@ public class CustomerServiceTest {
 
 	@Test
 	public void testGetOneClient_CustomerFound() throws CustomerNotFound {
-		// Arrange
 		long customerId = 1L;
 		CustomerEntity mockCustomer = new CustomerEntity();
 		mockCustomer.setId(customerId);
 		when(customerRepository.getCustomerEntityById(customerId)).thenReturn(mockCustomer);
-
-		// Act
 		CustomerModel result = customerService.getOneClient(customerId);
 
-		// Assert
 		assertNotNull(result);
 		assertEquals(customerId, result.getId());
 	}
 
 	@Test
 	public void testGetOneClient_CustomerNotFound() {
-		// Arrange
 		long customerId = 1L;
 		when(customerRepository.getCustomerEntityById(customerId)).thenReturn(null);
 
-		// Act & Assert
 		assertThrows(CustomerNotFound.class, () -> customerService.getOneClient(customerId));
 	}
 
 	@Test
 	public void testLogin_ValidCredentials() throws CustomerNotFound, IncorrectPassword {
-		// Arrange
 		String cardNumber = "1234567890123456";
 		int password = 1234;
 		CustomerEntity mockCustomer = new CustomerEntity();
@@ -69,10 +62,8 @@ public class CustomerServiceTest {
 		mockCustomer.setCode(passwordEncoder.encode(String.valueOf(password)));
 		when(customerRepository.getCustomerEntityByCardNumber(cardNumber)).thenReturn(mockCustomer);
 
-		// Act
 		boolean result = customerService.login(new LoginInputValue(cardNumber, password));
 
-		// Assert
 		assertTrue(result);
 	}
 
@@ -82,16 +73,13 @@ public class CustomerServiceTest {
 		LoginInputValue inputValue = new LoginInputValue();
 		inputValue.setCode(1234);
 		inputValue.setCard_number(cardNumber);
-		// Arrange
 		when(customerRepository.getCustomerEntityByCardNumber(cardNumber)).thenReturn(null);
 
-		// Act & Assert
 		assertThrows(CustomerNotFound.class, () -> customerService.login(new LoginInputValue(cardNumber, 1234)));
 	}
 
 	@Test
 	public void testLogin_IncorrectPassword() {
-		// Arrange
 		String cardNumber = "1234567890123456";
 		int correctPassword = 1234;
 		int incorrectPassword = 5678;
@@ -100,13 +88,11 @@ public class CustomerServiceTest {
 		mockCustomer.setCode(passwordEncoder.encode(String.valueOf(correctPassword)));
 		when(customerRepository.getCustomerEntityByCardNumber(cardNumber)).thenReturn(mockCustomer);
 
-		// Act & Assert
 		assertThrows(IncorrectPassword.class, () -> customerService.login(new LoginInputValue(cardNumber, incorrectPassword)));
 	}
 
 	@Test
 	public void testLoginAndShowInfoAboutClient_ValidCredentials() throws CustomerNotFound, IncorrectPassword, LoginFailed {
-		// Arrange
 		String cardNumber = "1234567890123456";
 		int password = 1234;
 		CustomerEntity mockCustomer = new CustomerEntity();
@@ -114,10 +100,8 @@ public class CustomerServiceTest {
 		mockCustomer.setCode(passwordEncoder.encode(String.valueOf(password)));
 		when(customerRepository.getCustomerEntityByCardNumber(cardNumber)).thenReturn(mockCustomer);
 
-		// Act
 		CustomerModel result = customerService.loginAndShowInfoAboutClient(new LoginInputValue(cardNumber, password));
 
-		// Assert
 		assertNotNull(result);
 		assertEquals(cardNumber, result.getCardNumer());
 	}
@@ -126,12 +110,10 @@ public class CustomerServiceTest {
 
 	@Test
 	public void testRegistration_PasswordNotHave4Digits() {
-		// Arrange
 		RegistrationInputValue registrationInputValue = new RegistrationInputValue();
 		registrationInputValue.setName("John Doe");
 		registrationInputValue.setCode(123);
 
-		// Act & Assert
 		assertThrows(PasswordNotHave4Digits.class, () -> customerService.registration(registrationInputValue));
 	}
 
